9e50095ad99f834fc30ed94570e8741c54c2f33c
Ardi Setiawan <ardisetiawanwindiarto@gmail.com>
2022-12-01 15:33:50 +0700

    push

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
     <div class="login-box-body">
         <div class="login-logo">
             <a href="{{url('/')}}">
-                <img src="{{asset('img/logo.png')}}" alt="" srcset="" width="100">
+                <img src="{{ url($setting->path_logo)}}" alt="" srcset="" width="100">
             </a>
           </div>
   
